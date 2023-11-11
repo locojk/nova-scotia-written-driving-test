d4d2390cbba552cfd3065efd48b7a62461ed8969
Zihao Liu <zh447118@dal.ca>
2023-11-11 10:48:24 -0400

    separte main and maps

---
+++ app/src/main/java/com/example/novascotiawrittendrivingtest/MapsActivity.kt
@@ -0,0 +1,129 @@
+package com.example.novascotiawrittendrivingtest
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
+import android.os.Bundle
+import android.os.Looper
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+    private var lastKnownLocation: Location? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+        createLocationRequest()
+        setupLocationCallback()
+    }
+
+    private fun setupLocationCallback() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                lastKnownLocation = locationResult.lastLocation
+                updateUIWithLocation(lastKnownLocation)
+            }
+        }
+    }
+
+    private fun updateUIWithLocation(location: Location?) {
+        location?.let {
+            val currentLatLng = LatLng(it.latitude, it.longitude)
+            mMap.clear()
+            mMap.addMarker(MarkerOptions().position(currentLatLng).title("Current Location"))
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+        }
+    }
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest.Builder(10000L) // Interval in milliseconds
+            .setPriority(Priority.PRIORITY_HIGH_ACCURACY)
+            .setMinUpdateDistanceMeters(5f) // Your desired minimum displacement between location updates
+            .build()
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        updateLocationUI()
+    }
+
+    private fun updateLocationUI() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            mMap.isMyLocationEnabled = true
+            startLocationUpdates()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    private fun startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED
+        ) {
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+        } else {
+            Toast.makeText(this, "Location permission not granted", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                updateLocationUI()
+            } else {
+                Toast.makeText(this, "Location permission not granted", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
